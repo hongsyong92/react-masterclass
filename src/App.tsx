6b772e864b5f38f8fde59a8afe0d330d222c4456
SYONG <ghdgus24@naver.com>
2021-12-06 18:01:17 +0900

    타입스크립트 form 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`hello ${value}`);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
