51eefd8cd82c049d344366a7551f8b608526e76e
SYONG <ghdgus24@naver.com>
2021-12-07 18:26:04 +0900

    일단 코인데이터  fetch 까지

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
+
+interface RouterParams {
+  coinId: string;
+}
+
+interface RouteState {
+  name: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<RouterParams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
+}
+
+export default Coin;
