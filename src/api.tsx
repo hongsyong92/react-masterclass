06eb25ed36d3c5aecc5bba7297aa5abaf4483996
SYONG <ghdgus24@naver.com>
2021-12-14 12:30:36 +0900

    react query end

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,19 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 export async function fetchCoins() {
-  const res = await fetch("https://api.coinpaprika.com/v1/coins");
+  const res = await fetch(`${BASE_URL}/coins`);
+  const json = await res.json();
+  return json;
+}
+
+export async function fetchCoinInfo(coinId: string) {
+  const res = await fetch(`${BASE_URL}/coins/${coinId}`);
+  const json = await res.json();
+  return json;
+}
+
+export async function fetchCoinTickers(coinId: string) {
+  const res = await fetch(`${BASE_URL}/tickers/${coinId}`);
   const json = await res.json();
   return json;
 }
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
