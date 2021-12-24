f4692a8ea04e3ab5f31e701238bb0680d31758d7
SYONG <ghdgus24@naver.com>
2021-12-24 15:33:29 +0900

    todo list를 위해 다 지움

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import Router from "./Router";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -71,14 +64,9 @@ a {
 `;
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen />
-      </ThemeProvider>
+      <GlobalStyle />
     </>
   );
 }
