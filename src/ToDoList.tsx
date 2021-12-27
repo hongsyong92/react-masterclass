1fe7671ad7ebea61e5a3e992d32780b9fbba8630
SYONG <ghdgus24@naver.com>
2021-12-27 16:24:50 +0900

    다지우고react-hook-form으로 todo 기본연습

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,127 +1,25 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-//   const [todo, setTodo] = useState("");
-//   const [todoError, setTodoError] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const { value } = event.currentTarget;
-//     setTodoError("");
-//     setTodo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (todo.length < 10) {
-//       setTodoError("더 길게 적으세요");
-//     }
-//     console.log(todo);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           onChange={onChange}
-//           value={todo}
-//           placeholder="할일을 적어주세요"
-//         />
-//         <button>ADD</button>
-//         {todoError !== "" ? todoError : null}
-//       </form>
-//     </div>
-//   );
-// }
-
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  todo: string;
 }
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "패스워드가 다릅니다" },
-        { shouldFocus: true }
-      );
-    }
-    setError("extraError", { message: "server problem" });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const onSubmit = (data: IForm) => {
+    console.log("add to do", data.todo);
+    setValue("todo", "");
   };
-  console.log(errors);
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onValid)}
-        style={{ display: "flex", flexDirection: "column" }}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("email", {
-            required: "무조건 입력해줘",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "naver.com 주소만 허용",
-            },
+          {...register("todo", {
+            required: "할일 적기 필수입니다.",
           })}
-          placeholder="email을 적어주세요"
+          placeholder="할일을 적어주세요"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "무조건 입력해줘",
-            validate: {
-              noNico: (value) => (value.includes("nico") ? "no nico" : true),
-              noNick: (value) => (value.includes("nick") ? "no nick" : true),
-            },
-          })}
-          placeholder="First Name 적어주세요"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "무조건 입력해줘" })}
-          placeholder="Last Name 적어주세요"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", {
-            required: "무조건 입력해줘",
-            minLength: 10,
-          })}
-          placeholder="username 적어주세요"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", {
-            required: "무조건 입력해줘",
-            minLength: {
-              value: 5,
-              message: "password is too short",
-            },
-          })}
-          placeholder="password 적어주세요"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", { required: "무조건 입력해줘" })}
-          placeholder="password1 적어주세요"
-        />
-        <span>{errors?.password1?.message}</span>
         <button>ADD</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
