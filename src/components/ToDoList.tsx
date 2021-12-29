2046d0ae25725b6c6d4f56ae0ce1ab87871d983b
SYONG <ghdgus24@naver.com>
2021-12-29 14:41:41 +0900

    selector 기본 사용 방식

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,22 +1,39 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>TO DOS</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
+        {toDo.map((item) => (
+          <ToDo key={item.id} {...item} />
         ))}
       </ul>
+      <hr />
+
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((item) => (
+          <ToDo key={item.id} {...item} />
+        ))}
+      </ul>
+      <hr />
+
+      <h2>Done</h2>
+      <ul>
+        {done.map((item) => (
+          <ToDo key={item.id} {...item} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
